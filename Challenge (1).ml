6850bf27e13ae9a5920b6d7a76cab42ec5c1b230
Thomas Jiralerspong <48162623+superkaiba@users.noreply.github.com>
2021-02-20 13:26:16 -0500

    add ocaml challenge

---
+++ Challenge (1).ml
@@ -0,0 +1,87 @@
+(*QUESTIONS*)
+let identity x =
+  x
+;;
+
+
+let rec reverse (l : 'a list) : 'a list = 
+  match l with
+  | [] -> []
+  | x::xs -> (reverse xs) @ [x]
+;; 
+  
+let palindrome (l: 'a list) : bool  =
+  let rev_l = reverse l in 
+  let rec compare l1 l2 = 
+    match l1, l2 with 
+    | [], [] -> true
+    | x::xs, y::ys -> 
+        if x = y then
+          compare xs ys
+        else
+          false
+    | _, _ -> false
+  in
+  compare l rev_l
+        
+;;
+
+let rec squeeze l  = 
+  let rec aux l1 l2 el =
+    match l1 with
+    | [] -> l2
+    | x::xs -> if x = el
+        then aux xs l2 x
+        else aux xs (l2@[x]) x
+  in
+  match l with
+  | [] -> [] 
+  | y::ys -> aux ys [y] y 
+;;
+
+
+let rec sprinkle l = 
+  let rec aux sofar remaining =
+    match remaining with
+    | [] -> [[]]
+    | x::xs -> aux [[x]] xs
+;;
+
+
+let rec countNodes (t : 'a tree) : int = 
+  raise NotImplemented
+;;
+
+
+let rec ironOut (t: int tree) : int list list = 
+  raise NotImplemented
+;;
+
+
+let fib max = 
+  raise NotImplemented
+;;
+
+let pascal max = 
+  let sum a b =
+    a + b 
+  in
+  let rec aux prev count =
+    
+    let l1 = prev @ [0] in
+    let l2 = 0::prev in 
+    aux [(List.map2 sum l1 l2)]
+  in
+  match max with
+  | 0 -> []
+  | 1 -> []
+  | 2 -> [[1]]
+  | x -> aux [1] 
+      
+;;
+
+let z = identity 3 ;;
+let a = reverse [1;2;3;4];;
+let b = palindrome [1;2;3;4;3;2;1] ;;
+let c = palindrome [1;2;3;4;5;3;2;1] ;;
+let d = squeeze [1;2;2;2;3;3;2;4;4];;
